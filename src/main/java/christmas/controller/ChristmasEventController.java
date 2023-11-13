b4bd25753d79d337d513c00ff1703806438f5bbc
jhva <junghun8158@naver.com>
2023-11-13 22:25:31 +0900

    :sparkles: feat: Controller 클래스 추가

---
+++ src/main/java/christmas/controller/ChristmasEventController.java
@@ -0,0 +1,19 @@
+package christmas.controller;
+
+import christmas.view.InputView;
+import christmas.view.OutView;
+
+public class ChristmasEventController {
+
+    private InputView inputView;
+
+    private OutView outView;
+
+    public ChristmasEventController() {
+        this.inputView = new InputView();
+        this.outView = new OutView();
+    }
+
+    public void run() {
+    }
+}
