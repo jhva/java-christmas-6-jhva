1cffa4252c4c00dbd1a9db72a28983060405b639
jhva <junghun8158@naver.com>
2023-11-13 23:15:07 +0900

    :sparkles: feat(ChristmasConst): 뷰에 쓰이게 될 상수 메시지 추가

---
+++ src/main/java/christmas/constant/ChristmasConst.java
@@ -0,0 +1,19 @@
+package christmas.constant;
+
+public class ChristmasConst {
+
+    public static final String START_MSG = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.\n"
+            + "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    public static final String MENU_VIEW_MSG = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+    public static final String EVENT_BENEFITS_MSG = "12월 3일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n";
+    public static final String BEFORE_ORDER_MSG = "<주문 메뉴>";
+    public static final String BEFORE_DISCOUNT_MSG = "<할인 전 총주문 금액>";
+    public static final String GIVEAWAY_MENU_MSG = "<증정 메뉴>";
+    public static final String BENEFITS_DETAILS_MSG = "<혜택 내역>";
+    public static final String TOTAL_BENEFITS_AMOUNT_MSG = "<총혜택 금액>";
+    public static final String TOTAL_AMOUNT_AFTER_DISCOUNT_MSG = "<할인 후 예상 결제 금액>";
+    public static final String EVENT_BADGE_MSG = "<12월 이벤트 배지>";
+    public static final String NOTHING_MSG = "없음";
+    public static final String WON = "%s 원";
+    public static final String DISCOUNT_WON = "-%s 원";
+}
