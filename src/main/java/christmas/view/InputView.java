03e63504d45245012496d5a4f4e6eee40af2ee00
jhva <junghun8158@naver.com>
2023-11-14 00:11:50 +0900

    :sparkles: feat(InputView): 뷰 클래스 추가

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,5 +1,15 @@
 package christmas.view;
 
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
 
+    public String commonMenuInput() {
+        return Console.readLine();
+    }
+
+    public int commonIntegerInput() {
+        return Integer.parseInt(Console.readLine());
+    }
 }
