533698d088f76a79d38a20aa1b0257c23498ab6e
jhva <junghun8158@naver.com>
2023-11-14 12:45:24 +0900

    :sparkles: feat(ChristmasConst): 공통 상수 추가

--- src/main/java/christmas/constant/ChristmasConst.java
+++ src/main/java/christmas/constant/ChristmasConst.java
@@ -14,6 +14,7 @@ public class ChristmasConst {
     public static final String TOTAL_AMOUNT_AFTER_DISCOUNT_MSG = "<할인 후 예상 결제 금액>";
     public static final String EVENT_BADGE_MSG = "<12월 이벤트 배지>";
     public static final String NOTHING_MSG = "없음";
+    public static final String SELECT_MENU_NAME_QUANTITY = "%s %d개";
     public static final String WON = "%s 원";
     public static final String DISCOUNT_WON = "-%s 원";
     public static final String COMMA = ",";
