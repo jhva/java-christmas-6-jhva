ec47b7f98ce1c76fc7aabe6f46abd3f77f7b1797
jhva <junghun8158@naver.com>
2023-11-14 19:33:50 +0900

    :sparkles: feat(ChristmasService): ChristmasBenefits 혜택 내역 추가

--- src/main/java/christmas/service/ChristmasEventService.java
+++ src/main/java/christmas/service/ChristmasEventService.java
@@ -2,10 +2,13 @@ package christmas.service;
 
 import static christmas.constant.ChristmasConst.BAR;
 
+import christmas.constant.ChristmasBenefits;
 import christmas.constant.MenuType;
 import christmas.dto.CalendarDto;
 import christmas.dto.MenuDto;
 import christmas.model.Menu;
+import christmas.model.TotalDiscountMenu;
+import christmas.model.TotalMenu;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -51,5 +54,28 @@ public class ChristmasEventService {
             menuDtos.add(menuDto);
         }
         return menuDtos;
+    }
+
+    public List<ChristmasBenefits> finderOfTotalMenuFromDiscount(CalendarDto calendar, TotalMenu menu,
+            boolean isGivenChampagne) {
+        List<ChristmasBenefits> allBenefits = new ArrayList<>();
+        TotalDiscountMenu discountMenu = null;
+        for (MenuDto menus : menu.getMenu()) {
+            discountMenu = new TotalDiscountMenu(calendar.isWeekend(), calendar.isWeekday(), calendar.isSpecial(),
+                    menus.menuQuantity(), isGivenChampagne, menus.menuType());
+            validatorBenfits(discountMenu, isGivenChampagne, allBenefits);
+        }
+        return allBenefits;
+    }
+
+    public void validatorBenfits(TotalDiscountMenu totalDiscountMenu, boolean isGivenChampagne,
+            List<ChristmasBenefits> allBenefits) {
+        ChristmasBenefits[] benefitsValue = ChristmasBenefits.values();
+        for (ChristmasBenefits befits : benefitsValue) {
+            befits.updateDiscountCounters(totalDiscountMenu);
+            if (befits.getCounter() > 0 && !allBenefits.contains(befits)) {
+                allBenefits.add(befits);
+            }
+        }
     }
 }
