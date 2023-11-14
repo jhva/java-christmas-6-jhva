3280cd109d0e65636da998fc4f120e3ecec01b3b
jhva <junghun8158@naver.com>
2023-11-15 07:38:30 +0900

    :recycle: refactor: 크리스마스 할인 혜택 상수 메시지 수정

--- src/main/java/christmas/constant/ChristmasBenefits.java
+++ src/main/java/christmas/constant/ChristmasBenefits.java
@@ -5,10 +5,10 @@ import christmas.model.TotalDiscountMenu;
 public enum ChristmasBenefits {
 
     CHRISTMAS("크리스마스 디데이 할인: -%s원", 0, 0),
-    WEEK("평일 할인: -%s", 0, 0),
-    WEEKEND("주말 할인: -%s", 0, 0),
-    SPECIAL("특별 할인: -%s", 0, 0),
-    GIVEN("증정 이벤트: -%s", 0, 0);
+    WEEK("평일 할인: -%s원", 0, 0),
+    WEEKEND("주말 할인: -%s원", 0, 0),
+    SPECIAL("특별 할인: -%s원", 0, 0),
+    GIVEN("증정 이벤트: -%s원", 0, 0);
 
     ChristmasBenefits(String message, int counter, int price) {
         this.counter = counter;
@@ -20,7 +20,8 @@ public enum ChristmasBenefits {
     private String message;
     private int price;
 
-    public void updateDiscountCounters(TotalDiscountMenu totalDiscountMenu) {
+    public void updateDiscountCounters(TotalDiscountMenu totalDiscountMenu, int christmasDiscount) {
+        ChristmasBenefits.CHRISTMAS.incrementCounter(christmasDiscount, christmasDiscount);
         ChristmasBenefits.WEEK.incrementCounter(totalDiscountMenu.getWeekdayAmount(),
                 totalDiscountMenu.getWeekdayAmount());
         ChristmasBenefits.WEEKEND.incrementCounter(totalDiscountMenu.getWeekendAmount(),
