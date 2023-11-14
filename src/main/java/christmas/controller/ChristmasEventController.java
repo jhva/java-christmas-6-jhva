46f1316692ecbc33d8c207719d8ad21b861e6ee1
jhva <junghun8158@naver.com>
2023-11-15 07:40:48 +0900

    :sparkles: feat: 토탈 금액 계산 기능 추가

--- src/main/java/christmas/controller/ChristmasEventController.java
+++ src/main/java/christmas/controller/ChristmasEventController.java
@@ -1,11 +1,10 @@
 package christmas.controller;
 
 import static christmas.constant.ChristmasConst.COMMA;
-import static christmas.constant.ChristmasConst.SELECT_MENU_NAME_QUANTITY;
+import static christmas.constant.ChristmasConst.DEFAULT_CHAMPAGNE_AMOUNT;
 import static christmas.utils.ChristmasAppUtils.formatPrice;
 import static christmas.utils.ChristmasAppUtils.splitByRegex;
 
-import christmas.constant.ChristmasBadge;
 import christmas.constant.ChristmasBenefits;
 import christmas.dto.CalendarDto;
 import christmas.dto.MenuDto;
@@ -18,6 +17,7 @@ import christmas.view.OutView;
 import java.util.List;
 
 public class ChristmasEventController {
+
 
     private final InputView inputView;
     private final OutView outView;
@@ -34,25 +34,12 @@ public class ChristmasEventController {
         TotalMenu menu = menuSelector();
         int totalAmount = totalAmountAndSelectedMenu(menu);
         boolean isGivenChampagne = menu.isTotalAmountSufficient(totalAmount);
-        givenChampagne(isGivenChampagne);
-        List<ChristmasBenefits> totalDiscountMenu = christmasEventService.finderOfTotalMenuFromDiscount(calendar, menu,
-                isGivenChampagne);
-        ChristmasBenefitsDetails benefitsDetails = new ChristmasBenefitsDetails(totalDiscountMenu);
+        outView.giveawayMenuMsg(isGivenChampagne);
+        ChristmasBenefitsDetails benefitsDetails = new ChristmasBenefitsDetails(
+                christmasEventService.finderOfTotalMenuFromDiscount(calendar, menu, isGivenChampagne));
         totalDiscountMenuCheck(benefitsDetails, calendar.christmasCount());
-        int benefitAmount = benefitsDetails.calculateTotalBenefits();
-        eventParticipationBadge(benefitAmount);
-
+        totalAmountAndBadge(benefitsDetails, totalAmount, isGivenChampagne);
     }
-
-    private void givenChampagne(boolean isGivenChampagne) {
-        outView.giveawayMenuMsg();
-        if (!isGivenChampagne) {
-            outView.nothingMsg();
-            return;
-        }
-        outView.selectedMenuMsg(SELECT_MENU_NAME_QUANTITY, 1);
-    }
-
 
     private TotalMenu menuSelector() {
         outView.menuViewMsg();
@@ -75,7 +62,6 @@ public class ChristmasEventController {
     public int totalAmountAndSelectedMenu(TotalMenu totalMenu) {
         outView.beforeOrderingMsg();
         selectedMenu(totalMenu);
-        System.out.println();
         outView.totalOrderAmountBeforeDiscountMsg();
         int totalAmount = totalMenu.selectedViewMenuReturnTotalPrice();
         outView.numberTotalPriceFormatWonMsg(formatPrice(totalAmount));
@@ -88,14 +74,27 @@ public class ChristmasEventController {
         }
     }
 
-    private void totalDiscountMenuCheck(ChristmasBenefitsDetails benefitsDetails, int christmasDiscount) {
-        outView.benefitsDetailsMsg();
-//        benefitsDetails.test();
+    public void totalAmountAndBadge(ChristmasBenefitsDetails benefitsDetails, int totalAmount,
+            boolean isGivenChampagne) {
+        int benefitAmount = benefitsDetails.calculateTotalBenefits();
+        int expectedAmount = benefitsDetails.calculateTotalBenefits();
+        outView.totalBenefitsAmountMsg();
+        outView.discountPriceFormatMsg(formatPrice(benefitAmount));
+        if (isGivenChampagne) {
+            expectedAmount -= DEFAULT_CHAMPAGNE_AMOUNT;
+        }
+        outView.totalAmountAfterDiscountMsg(totalAmount, expectedAmount);
+        outView.eventBadgeMsg(benefitAmount);
     }
 
-    public void eventParticipationBadge(int benefitAmount) {
-        String badge = ChristmasBadge.getBadge(benefitAmount);
-        outView.eventBadgeMsg();
-        System.out.println(badge);
+    private void totalDiscountMenuCheck(ChristmasBenefitsDetails benefitsDetails, int christmasDiscount) {
+        List<ChristmasBenefits> benefits = benefitsDetails.getBenefits();
+        outView.benefitsDetailsMsg();
+        if (benefits.size() == 0) {
+            outView.nothingMsg();
+        }
+        for (ChristmasBenefits benefit : benefitsDetails.getBenefits()) {
+            System.out.println(String.format(benefit.getMessage(), formatPrice(benefit.getPrice())));
+        }
     }
 }
