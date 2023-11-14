7519893ccdc6fc245f10dcde4f81c983ba8b0904
jhva <junghun8158@naver.com>
2023-11-14 12:03:01 +0900

    :sparkles: feat(ChristmasController): 달력 및 메뉴 입력 받기 기능 추가

--- src/main/java/christmas/controller/ChristmasEventController.java
+++ src/main/java/christmas/controller/ChristmasEventController.java
@@ -1,19 +1,48 @@
 package christmas.controller;
 
+import static christmas.constant.ChristmasConst.COMMA;
+import static christmas.utils.ChristmasAppUtils.splitByRegex;
+
+import christmas.dto.CalendarDto;
+import christmas.dto.MenuDto;
+import christmas.model.Calendar;
+import christmas.service.ChristmasEventService;
 import christmas.view.InputView;
 import christmas.view.OutView;
+import java.util.List;
 
 public class ChristmasEventController {
 
-    private InputView inputView;
+    private final InputView inputView;
 
-    private OutView outView;
+    private final OutView outView;
+    private final ChristmasEventService christmasEventService;
 
-    public ChristmasEventController() {
-        this.inputView = new InputView();
-        this.outView = new OutView();
+    public ChristmasEventController(InputView inputView, OutView outView) {
+        this.inputView = inputView;
+        this.outView = outView;
+        this.christmasEventService = new ChristmasEventService();
     }
 
     public void run() {
+        outView.starterViewMsg();
+        CalendarDto calendar = reservationVisitDay();
+        List<MenuDto> menu = menuSelector();
+    }
+
+    private List<MenuDto> menuSelector() {
+        outView.menuViewMsg();
+        String menu = inputView.commonMenuInput();
+        String[] menuParseWithRegex = splitByRegex(menu, COMMA);
+        List<MenuDto> menuDto = christmasEventService.createMenu(menuParseWithRegex);
+        return menuDto;
+    }
+
+    private CalendarDto reservationVisitDay() {
+        int day = inputView.commonIntegerInput();
+        Calendar calendar = new Calendar(day);
+        CalendarDto calendarDto = CalendarDto.fromCalendar(calendar);
+        christmasEventService.confirmCalendar(calendarDto);
+        return calendarDto;
     }
 }
