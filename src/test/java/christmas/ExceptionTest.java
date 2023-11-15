c7cc066ee7cc36d0aea576debb6bd2093564c07a
jhva <junghun8158@naver.com>
2023-11-15 16:19:00 +0900

    :white_check_mark: test: 메뉴 예외 처리 테스트 완료

--- src/test/java/christmas/ExceptionTest.java
+++ src/test/java/christmas/ExceptionTest.java
@@ -13,7 +13,7 @@ public class ExceptionTest extends NsTest {
     @Test
     void 날짜_재입력() {
         assertThatThrownBy(() -> {
-            runException("0,45");
+            runException("0", "45");
             assertThat(output()).contains("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.")
                     .isInstanceOf(UserInputException.class);
         });
@@ -22,7 +22,7 @@ public class ExceptionTest extends NsTest {
     @Test
     void 메뉴_테스트() {
         assertThatThrownBy(() -> {
-            runException("12,ㅁㄴㅇㄹ-1,ㅁㄴㅇ3");
+            runException("12", "ㅁㄴㅇㄹ-1,ㅁㄴㅇ3");
             assertThat(output()).contains("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.")
                     .isInstanceOf(UserInputException.class);
         });
@@ -31,8 +31,17 @@ public class ExceptionTest extends NsTest {
     @Test
     void 메뉴_중복_테스트() {
         assertThatThrownBy(() -> {
-            runException("티본스테이크-1,티본스테이크-1");
+            runException("23", "티본스테이크-1,티본스테이크-1");
             assertThat(output()).contains("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.")
+                    .isInstanceOf(UserInputException.class);
+        });
+    }
+
+    @Test
+    void 음료_입력_테스트() {
+        assertThatThrownBy(() -> {
+            runException("23", "제로콜라-3");
+            assertThat(output()).contains("[ERROR] 음료만 있는 주문은 처리할 수 없습니다. 다시 입력 해주세요.")
                     .isInstanceOf(UserInputException.class);
         });
     }
@@ -40,8 +49,8 @@ public class ExceptionTest extends NsTest {
     @Test
     void 메뉴_개수_테스트() {
         assertThatThrownBy(() -> {
-            runException("12,티본스테이크-23");
-            assertThat(output()).contains("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.")
+            runException("23", "티본스테이크-19,타파스-2");
+            assertThat(output()).contains("[ERROR] 메뉴의 총 개수 20개를 초과하였습니다. 다시 입력해 주세요")
                     .isInstanceOf(UserInputException.class);
         });
     }
