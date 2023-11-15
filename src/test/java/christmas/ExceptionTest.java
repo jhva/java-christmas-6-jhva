56a957976fa7e4924a77477ff3a8e05d3c20aa28
jhva <junghun8158@naver.com>
2023-11-15 12:24:03 +0900

    :white_check_mark: test: 날짜 다시 입력 받기 테스트 완료

--- src/test/java/christmas/ExceptionTest.java
+++ src/test/java/christmas/ExceptionTest.java
@@ -1,5 +1,6 @@
 package christmas;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -16,6 +17,14 @@ public class ExceptionTest extends NsTest {
             runException("0,45");
             assertThat(output()).contains("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.")
                     .isInstanceOf(UserInputException.class);
+        });
+    }
+
+    @Test
+    void 날짜_다시_입력_받는_여지_테스트() {
+        assertSimpleTest(() -> {
+            runException("27");
+            assertThat(output()).contains("26일 이후 부터는 크리스마스 혜택을 받을 수 없습니다. 진행하시겠습니까?\n1.계속 2.돌아가기");
         });
     }
 
